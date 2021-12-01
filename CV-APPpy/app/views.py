3048ca7af367fa875b815bc02cec6f5604942b5a
Nemanja Pavlovic <pavlovicn88@gmail.com>
2021-12-01 09:34:56 +0100

    User Login is created

--- CV-APPpy/app/views.py
+++ CV-APPpy/app/views.py
@@ -3,7 +3,10 @@ Definition of views.
 """
 
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login ,logout
 from django.http import HttpRequest
 
 def home(request):
@@ -17,6 +20,30 @@ def home(request):
             'year':datetime.now().year,
         }
     )
+
+def login_page(request):
+    if request.method=='POST':   
+        username=request.POST.get('username')
+        pasword=request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,'user does not exists ')
+        
+        user= authenticate(request,username=username,password=pasword)
+       
+
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else :
+             messages.error(request,'username or password does not exist ')
+
+
+    context={}
+    return render(request,'app/login_registrate00.html')
+
 
 def contact(request):
     """Renders the contact page."""
