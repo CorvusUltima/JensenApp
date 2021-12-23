e1c425bb76c817bd6b7e61e65f3ff6b46b0d7da5
sam-1998 <sosam@student.chalmers.se>
2021-12-23 16:05:29 +0100

    1.05a (Made update profile take id) + 1.07 Fixed flash messages when typing wrong username and password when trying to login

--- CV-APPpy/ProfilePages/views.py
+++ CV-APPpy/ProfilePages/views.py
@@ -13,37 +13,33 @@ from ProfilePages.models import Profile
 def profiles(request):
      return render(request,'ProfilePages/profiles.html')
 
-def profile_page(request,pk):
-    if request.user.is_authenticated:
+def profile_page(request, pk):
+    if((str(request.user.profile.id) == pk) | request.user.is_superuser):
         assignments = []
         profile = Profile.objects.get(id=pk)
         for assignment in Assignment.objects.filter(host=request.user):
             assignments.append(assignment)
         context={'assignments':assignments,'profile':profile}
-    
         return render(request,'ProfilePages/profile-page.html',context)
-    
-
     return redirect('login')
 
 
-def profile_update(request):
-
-
-    profile=Profile.objects.get(user=request.user)
-    form=CreateProfileForm(instance=profile)
-    if request.method=='POST':
-        form=CreateProfileForm(request.POST,instance=profile)
-        if form.is_valid():
-           form.save()
-        return redirect('profile')
-           
-    context={'form': form}
-    return render(request,'ProfilePages/profile-update.html',context)
+def profile_update(request, pk):
+    if((str(request.user.profile.id) == pk) | request.user.is_superuser):
+        profile = Profile.objects.get(id=pk)
+        form = CreateProfileForm(instance=profile)
+        if request.method=='POST':
+            form=CreateProfileForm(request.POST,instance=profile)
+            if form.is_valid():
+                form.save()
+                return redirect('profile')
+        context={'form': form}
+        return render(request,'ProfilePages/profile-update.html',context)
+    else:
+        return redirect('home')
 
 def test(request):
     ob=353
     context={'ob':ob}
-
     return render(request,'ProfilePages/test.html',context)
 
